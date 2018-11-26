a75e4fee409c36affb38807de3961421726e0228
Tiago Martins <tmartinslcc@gmail.com>
2018-11-26 01:44:39 +0000

    add you api key

--- app/src/main/java/com/tmartins/task/moviesapp/core/Config.java
+++ app/src/main/java/com/tmartins/task/moviesapp/core/Config.java
@@ -4,6 +4,7 @@ public class Config {
 
     public static final String API_URL = "https://api.themoviedb.org/" + Config.API_VERSION;
 
+    //TODO add your themoviedb api key
     public static final String API_KEY = "";
 
     private static final String API_VERSION = "3/";
